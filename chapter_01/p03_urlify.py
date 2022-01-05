a68e37061ab1230892944d45caf71bc1a809ae47
Ngo The Hieu <60764766+NgoTheHieu@users.noreply.github.com>
2022-01-05 07:57:56 +0700

    Update p03_urlify.py
    Add Title

--- chapter_01/p03_urlify.py
+++ chapter_01/p03_urlify.py
@@ -1,6 +1,14 @@
 # O(N)
 import unittest
 
+"""
+
+URLify: Write a method to replace all spaces in a string with '%20'. You may assume that the string
+has sufficient space at the end to hold the additional characters, and that you are given the "true"
+length of the string. (Note: If implementing in Java, please use a character array so that you can
+perform this operation in place.) 
+
+"""
 
 def urlify_algo(string, length):
     """replace spaces with %20 and removes trailing spaces"""
