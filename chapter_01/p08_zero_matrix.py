e52d659662f9f951d853c503f817c1a9329b5daf
Ngo The Hieu <60764766+NgoTheHieu@users.noreply.github.com>
2022-01-05 08:00:40 +0700

    Update p08_zero_matrix.py
    Add Title

--- chapter_01/p08_zero_matrix.py
+++ chapter_01/p08_zero_matrix.py
@@ -2,6 +2,11 @@
 import unittest
 from copy import deepcopy
 
+"""
+
+Rotate Matrix: Given an image represented by an NxN matrix, where each pixel in the
+image is 4 bytes, write a method to rotate the image by 90 degrees. Can you do this in place? 
+"""
 
 def zero_matrix(matrix):
     m = len(matrix)
