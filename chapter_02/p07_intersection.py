e9de39e272e67bd55a0227f33bc8088e7454665c
Ngo The Hieu <60764766+NgoTheHieu@users.noreply.github.com>
2022-01-09 14:20:46 +0700

    Update p07_intersection.py

--- chapter_02/p07_intersection.py
+++ chapter_02/p07_intersection.py
@@ -1,5 +1,11 @@
 from chapter_02.linked_list import LinkedList
 
+"""
+Intersection: Given two (singly) linked lists, determine if the two lists intersect. Return the 
+intersecting node. Note that the intersection is defined based on reference, not value. That is, if the 
+kth node of the first linked list is the exact same node (by reference) as the jth node of the second 
+linked list, then they are intersecting.
+"""
 
 def intersection(list1, list2):
     if list1.tail is not list2.tail:
