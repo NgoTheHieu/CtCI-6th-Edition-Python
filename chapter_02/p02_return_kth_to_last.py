740662610fb1222a3b99d7871c8ed06b7b4dd566
Ngo The Hieu <60764766+NgoTheHieu@users.noreply.github.com>
2022-01-09 14:16:13 +0700

    Update p02_return_kth_to_last.py

--- chapter_02/p02_return_kth_to_last.py
+++ chapter_02/p02_return_kth_to_last.py
@@ -1,5 +1,13 @@
 from chapter_02.linked_list import LinkedList
 
+"""
+Delete Middle Node: Implement an algorithm to delete a node in the middle (i.e., any node but 
+the first and last node, not necessarily the exact middle) of a singly linked list, given only access to 
+that node. 
+EXAMPLE 
+lnput:the node c from the linked list a->b->c->d->e->f 
+Result: nothing is returned, but the new linked list looks like a ->b->d->e->f
+"""
 
 def kth_to_last(ll, k):
     runner = current = ll.head
