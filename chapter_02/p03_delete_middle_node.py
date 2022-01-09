8cc4aed969590b40620e19aa20efd97c42847e64
Ngo The Hieu <60764766+NgoTheHieu@users.noreply.github.com>
2022-01-09 14:17:04 +0700

    Update p03_delete_middle_node.py

--- chapter_02/p03_delete_middle_node.py
+++ chapter_02/p03_delete_middle_node.py
@@ -1,5 +1,13 @@
 from chapter_02.linked_list import LinkedList
 
+"""
+Delete Middle Node: Implement an algorithm to delete a node in the middle (i.e., any node but 
+the first and last node, not necessarily the exact middle) of a singly linked list, given only access to 
+that node. 
+EXAMPLE 
+lnput:the node c from the linked list a->b->c->d->e->f 
+Result: nothing is returned, but the new linked list looks like a ->b->d->e->f
+"""
 
 def delete_middle_node(node):
     node.value = node.next.value
