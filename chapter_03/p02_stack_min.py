e3d6b4aa87a67d1c695d69bf4c7130dbb13a0793
Ngo The Hieu <60764766+NgoTheHieu@users.noreply.github.com>
2022-01-17 12:10:20 +0700

    Update p02_stack_min.py

--- chapter_03/p02_stack_min.py
+++ chapter_03/p02_stack_min.py
@@ -1,5 +1,9 @@
 from chapter_03.stack import Stack
 
+"""
+Stack Min: How would you design a stack which, in addition to push and pop, has a function min 
+which returns the minimum element? Push, pop and min should all operate in 0(1) time. 
+"""
 
 class MinStack(Stack):
     def __init__(self):
