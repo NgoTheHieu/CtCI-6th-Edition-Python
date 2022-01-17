45e3ae9b686df622a7c7344da0637c777e1bbb8c
Ngo The Hieu <60764766+NgoTheHieu@users.noreply.github.com>
2022-01-17 12:15:07 +0700

    Update p05_sort_stack.py

--- chapter_03/p05_sort_stack.py
+++ chapter_03/p05_sort_stack.py
@@ -3,6 +3,12 @@ import unittest
 
 from chapter_03.stack import Stack
 
+"""
+Sort Stack: Write a program to sort a stack such that the smallest items are on the top. You can use 
+an additional temporary stack, but you may not copy the elements into any other data structure 
+(such as an array). The stack supports the following operations: push, pop, peek, and is Empty. 
+Hints:# 15, #32, #43
+"""
 
 class SortedStack(Stack):
     def __init__(self):
