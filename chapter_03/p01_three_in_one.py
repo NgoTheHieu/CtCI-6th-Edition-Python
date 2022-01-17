d61700d4572381a866eb85c22fd5930dd134e1ee
Ngo The Hieu <60764766+NgoTheHieu@users.noreply.github.com>
2022-01-17 12:10:00 +0700

    Update p01_three_in_one.py

--- chapter_03/p01_three_in_one.py
+++ chapter_03/p01_three_in_one.py
@@ -1,5 +1,9 @@
 import pytest
 
+"""
+Three in One: Describe how you could use a single array to implement three stacks. 
+Hints: #2, #72, #38, #58
+"""
 
 class MultiStack:
     def __init__(self, stack_size, number_of_stacks):
