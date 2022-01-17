d6d0ea8d5c4ec76a87e4ea44bcee1df5d34d3d98
Ngo The Hieu <60764766+NgoTheHieu@users.noreply.github.com>
2022-01-17 12:11:14 +0700

    Update p04_queue_via_stacks.py

--- chapter_03/p04_queue_via_stacks.py
+++ chapter_03/p04_queue_via_stacks.py
@@ -3,6 +3,10 @@ import unittest
 
 from chapter_03.stack import Stack
 
+"""
+Queue via Stacks: Implement a MyQueue class which implements a queue using two stacks. 
+Hints: #98, #7 74 
+"""
 
 class MyQueue:
     def __init__(self):
