5f20a47648e98d49d0672da1f9e66b71cf724935
Ngo The Hieu <60764766+NgoTheHieu@users.noreply.github.com>
2022-01-17 12:43:10 +0700

    Update p02_minimal_tree.py

--- chapter_04/p02_minimal_tree.py
+++ chapter_04/p02_minimal_tree.py
@@ -1,3 +1,6 @@
+"""
+Minimal Tree: Given a sorted (increasing order) array with unique integer elements, write an algorithm to create a binary search tree with minimal height. 
+"""
 class Node:
     def __init__(self, item):
         self.right = None
