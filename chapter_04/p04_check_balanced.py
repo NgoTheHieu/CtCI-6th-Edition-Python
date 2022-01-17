3c568705405b236e017c3302a21cf68e110c569b
Ngo The Hieu <60764766+NgoTheHieu@users.noreply.github.com>
2022-01-17 13:44:25 +0700

    Update p04_check_balanced.py

--- chapter_04/p04_check_balanced.py
+++ chapter_04/p04_check_balanced.py
@@ -4,6 +4,11 @@ class BinaryNode:
         self.left = None
         self.right = None
 
+"""
+Check Balanced: Implement a function to check if a binary tree is balanced. For the purposes of 
+this question, a balanced tree is defined to be a tree such that the heights of the two subtrees of any 
+node never differ by more than one. 
+"""
 
 # Version 2:
 # Traverse the tree and track the largest and smallest depth of each leaf node.
